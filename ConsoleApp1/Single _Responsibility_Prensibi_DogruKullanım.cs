9e74e9f989ab299da72e00218986c3bf1c33cb34
Kaan Kösen <121513474+kaankosen@users.noreply.github.com>
2023-03-29 09:28:34 +0300

    S.O.L.I.D
    Single Responsibility Prensibi

---
+++ ConsoleApp1/Single _Responsibility_Prensibi_DogruKullanım.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class BackEndDeveloper
+
+{
+    public void CSharpKodYaz()
+    {
+        Console.WriteLine("Ben C# yazabilirim.");
+    }
+}
+
+public class FrontEndDeveloper
+{
+    public void JavaScriptKodYaz()
+    {
+        Console.WriteLine("Ben JS yazabilirim.");
+    }
+
+    public void CSSKodYaz()
+    {
+        Console.WriteLine("Ben CSS yazabilirim.");
+    }
+}
+
+public class SQLDeveloper
+{
+    public void SQLKodYaz()
+    {
+        Console.WriteLine("Ben SQL yazabilirim.");
+    }
+}
---
+++ ConsoleApp1/Single _Responsibility_Prensibi_YanlısKullanım.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class FullStackDeveloper
+{
+    public void CSharpKodYaz()
+    {
+        Console.WriteLine("Ben C# yazabiliyorum.");
+    }
+
+    public void SQLKodYaz()
+    {
+        Console.WriteLine("Ben SQL yazabiliyorum.");
+    }
+
+    public void JavaScriptKodYaz()
+    {
+        Console.WriteLine("Ben JS yazabiliyorum.");
+    }
+
+    public void CSSKodYaz()
+    {
+        Console.WriteLine("Ben CSS yazabiliyorum.");
+    }
+}
